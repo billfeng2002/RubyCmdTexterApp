e211d14d391f511001c7f19bc706cc906c2e78c3
pranik chainani <pranikc@gmail.com>
2021-06-21 15:55:17 -0400

    handle change in directories

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,9 @@ require 'bundler'
 Bundler.require
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
-require_all 'lib'
+
+if Dir.pwd.split("/")[-1] == "config"
+  require_all '../lib'
+else
+  require_all 'lib'
+end
