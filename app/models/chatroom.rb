457e661a98ba7fa1c0a41a7c40f9104f7b89794e
Bill <billfeng2002@gmail.com>
2021-06-22 22:14:10 -0700

    add relational macros

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,3 +1,4 @@
 class Chatroom < ActiveRecord::Base
-
+    has_many :users
+    has_many :messages
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
-
+    has_many :chatrooms
+    has_many :messages
 end
