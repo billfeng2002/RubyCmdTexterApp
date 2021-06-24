b8926f436a4aed35f8fbc1004348584f59890ef7
Bill <billfeng2002@gmail.com>
2021-06-24 16:16:36 -0700

    refactor and format

--- app/models/app.rb
+++ app/models/app.rb
@@ -7,60 +7,57 @@ class App
   end
 
   def view_general_options
-    choices=["Edit Account Info", "View My Chat Rooms", "Join a Chat Room by Code", "Create New Chat Room", "Log Out"]
-    response=App.display_menu_and_get_input(choices, show_name: "Hi #{self.user.first_name}!")
+    choices = ["Edit Account Info", "View My Chat Rooms", "Join a Chat Room by Code", "Create New Chat Room", "Log Out"]
+    response = App.display_menu_and_get_input(choices, show_name: "Hi #{self.user.first_name}!")
     case response
     when 0
-        puts "Editing account info..."
-        self.edit_account_info
+      puts "Editing account info..."
+      self.edit_account_info
     when 1
-      puts "Displaying chatrooms"
-      rooms = self.get_chatrooms
-      room_codes = rooms.map { |each_room| each_room.room_code }
-      room_names = rooms.map { |each_room| each_room.name }
-
-      display_rooms = rooms.map do |each_room|
-        "#{each_room.name} (#{each_room.room_code})"
-      end
-
-      display_rooms << "Go back"
-
-      response = App.display_menu_and_get_input(choices=display_rooms, message="Choose a room")
-
-      if response == display_rooms.size - 1
-        self.view_general_options
-      else
-        current_room = display_rooms[response]
-        # next screen
-      end
+      puts "Listing chatrooms...."
+      self.chatroom_listing
     when 2
-        self.join_chatroom
+      self.join_chatroom
     when 3
-        self.create_chatroom
+      self.create_chatroom
     when 4
-        self.clear_data
-        self.main_sequence
+      self.clear_data
+      self.main_sequence
     end
   end
 
-  def get_chatrooms
-    self.user.chatrooms
+  def chatroom_listing
+    rooms = self.user.chatrooms
+    display_rooms = rooms.map do |room|
+      "#{room.name} (#{room.room_code})"
+    end
+
+    display_rooms << "Go back"
+
+    response = App.display_menu_and_get_input(display_rooms, "Choose a room to inspect, or go back:")
+
+    if response == rooms.size
+      self.view_general_options
+    else
+      current_room = display_rooms[response]
+      # next screen
+    end
   end
 
   def create_chatroom
     puts "Creating new chatroom..."
     print "Enter Room Name:"
-    room_name=App.get_non_empty_input("Enter Room Name:")
+    room_name = App.get_non_empty_input("Enter Room Name:")
     print "Enter Room Code:"
-    room_code=App.get_non_empty_input("Enter Room Code:")
-    while(Chatroom.find_by(room_code: room_code)!=nil)
-        puts "Room code already taken! Try again."
-        print "Enter Room Code:"
-        room_code=$stdin.gets.chomp
+    room_code = App.get_non_empty_input("Enter Room Code:")
+    while (Chatroom.find_by(room_code: room_code) != nil)
+      puts "Room code already taken! Try again."
+      print "Enter Room Code:"
+      room_code = $stdin.gets.chomp
     end
     print "Enter Room Password(Press enter if none):"
-    room_password=$stdin.gets.chomp
-    chatroom=Chatroom.create(name: room_name, password: room_password, room_code: room_code, owner_id: self.user.id)
+    room_password = $stdin.gets.chomp
+    chatroom = Chatroom.create(name: room_name, password: room_password, room_code: room_code, owner_id: self.user.id)
     Userchatroom.create(user_id: self.user.id, chatroom_id: chatroom.id)
     puts "Chatroom #{room_name} (code: #{room_code}) created!"
     sleep(1)
@@ -69,75 +66,74 @@ class App
 
   def join_chatroom
     print "Enter room code:"
-    room_code=$stdin.gets.chomp
+    room_code = $stdin.gets.chomp
     print "Enter room password(Enter if none):"
-    password=$stdin.gets.chomp
+    password = $stdin.gets.chomp
 
-    result=Chatroom.find_chatroom(code: room_code, password: password)
+    result = Chatroom.find_chatroom(code: room_code, password: password)
 
-    if(!result)
-        puts "Room not found."
+    if (!result)
+      puts "Room not found."
     elsif (self.user.chatrooms.include?(result))
-        puts "Already in this room!"
+      puts "Already in this room!"
     else
-        Userchatroom.create(user_id: self.user.id, chatroom_id: result.id)
-        puts "Room #{result.name} (code: #{room_code}) successfully joined!"
+      Userchatroom.create(user_id: self.user.id, chatroom_id: result.id)
+      puts "Room #{result.name} (code: #{room_code}) successfully joined!"
     end
     sleep(1)
     self.view_general_options
   end
 
   def edit_account_info
-    choices=["Change password", "Change first name", "Change last name", "Done"]
-    response=App.display_menu_and_get_input(choices, show_name: "Name: #{self.user.full_name}  Username: #{self.user.username}  Password: ********")
+    choices = ["Change password", "Change first name", "Change last name", "Done"]
+    response = App.display_menu_and_get_input(choices, show_name: "Name: #{self.user.full_name}  Username: #{self.user.username}  Password: ********")
     case response
     when 0
-        print "Old password:"
-        password=$stdin.gets.chomp
-        if(password==self.user.password)
-            puts "Password Verified!"
-            puts "Enter new password:"
-            self.user.password=App.get_non_empty_input("Enter new password:")
-            self.user.save
-            puts "Password saved!"
-        else
-            puts "Password incorrect"
-        end
-        self.edit_account_info
+      print "Old password:"
+      password = $stdin.gets.chomp
+      if (password == self.user.password)
+        puts "Password Verified!"
+        puts "Enter new password:"
+        self.user.password = App.get_non_empty_input("Enter new password:")
+        self.user.save
+        puts "Password saved!"
+      else
+        puts "Password incorrect"
+      end
+      self.edit_account_info
     when 1
-        print "New First Name:"
-        first_name=$stdin.gets.chomp
-        print "Are you sure you want to change your name to #{first_name}? (type Y for yes)"
-        confirmation=$stdin.gets.chomp
-        if(confirmation=='Y')
-            self.user.first_name=first_name
-            self.user.save
-            puts "First name changed..."
-        else
-            puts "Operation cancelled..."
-        end
-        self.edit_account_info
+      print "New First Name:"
+      first_name = $stdin.gets.chomp
+      print "Are you sure you want to change your name to #{first_name}? (type Y for yes)"
+      confirmation = $stdin.gets.chomp
+      if (confirmation == "Y")
+        self.user.first_name = first_name
+        self.user.save
+        puts "First name changed..."
+      else
+        puts "Operation cancelled..."
+      end
+      self.edit_account_info
     when 2
-        print "New Last Name:"
-        last_name=$stdin.gets.chomp
-        print "Are you sure you want to change your name to #{last_name}? (type Y for yes)"
-        confirmation=$stdin.gets.chomp
-        if(confirmation=='Y')
-            self.user.last_name=last_name
-            self.user.save
-            puts "First name changed..."
-        else
-            puts "Operation cancelled..."
-        end
-        self.edit_account_info
+      print "New Last Name:"
+      last_name = $stdin.gets.chomp
+      print "Are you sure you want to change your name to #{last_name}? (type Y for yes)"
+      confirmation = $stdin.gets.chomp
+      if (confirmation == "Y")
+        self.user.last_name = last_name
+        self.user.save
+        puts "First name changed..."
+      else
+        puts "Operation cancelled..."
+      end
+      self.edit_account_info
     when 3
-        self.view_general_options
+      self.view_general_options
     end
-
   end
 
   def clear_data
-    self.user=nil
+    self.user = nil
     self.current_room = nil
   end
 
@@ -154,29 +150,29 @@ class App
       return
     end
   end
-  
+
   def login_sequence
     puts "Logging in...."
-    login_count=0
-    while(login_count < 3)    
-        print("Enter your username:")
-        username=$stdin.gets.chomp
-        print("Enter your password:")
-        password=$stdin.gets.chomp
-        self.user=User.check_login(username:username, password:password)
-        if(self.user) #user was found
-            break
-        else
-            puts "Username or password incorrect."
-        end
-        login_count+=1
+    login_count = 0
+    while (login_count < 3)
+      print("Enter your username:")
+      username = $stdin.gets.chomp
+      print("Enter your password:")
+      password = $stdin.gets.chomp
+      self.user = User.check_login(username: username, password: password)
+      if (self.user) #user was found
+        break
+      else
+        puts "Username or password incorrect."
+      end
+      login_count += 1
     end
-    if(login_count==3)
-        #return to main menu, failed 3 times
-        puts "Login failed 3 times, returning to main menu..."
-        self.main_sequence
+    if (login_count == 3)
+      #return to main menu, failed 3 times
+      puts "Login failed 3 times, returning to main menu..."
+      self.main_sequence
     else
-        self.view_general_options
+      self.view_general_options
     end
   end
 
@@ -228,9 +224,9 @@ class App
     str.chars.detect { |ch| !chars.include?(ch) }.nil?
   end
 
-  def self.get_non_empty_input(prompt="")
+  def self.get_non_empty_input(prompt = "")
     input = ""
-    input=$stdin.gets.chomp
+    input = $stdin.gets.chomp
     while (input == "")
       puts "Please provide a non-empty input!"
       print prompt
@@ -239,10 +235,10 @@ class App
     return input
   end
 
-  def self.display_menu_and_get_input(output, message = "Please select one of the following options (type the number):", show_name:nil)
-    puts '#############################'
-    if(show_name)
-        puts "#{show_name}"
+  def self.display_menu_and_get_input(output, message = "Please select one of the following options (type the number):", show_name: nil)
+    puts "#############################"
+    if (show_name)
+      puts "#{show_name}"
     end
     puts message
     output.each_with_index { |item, index| puts "#{index + 1}. #{item}" }
