a1419685fc5e1d009f0561a197d0ecbabc058bfb
Bill <billfeng2002@gmail.com>
2021-06-24 01:00:42 -0700

    add some functions/todo

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,9 +1,16 @@
 class App
+    attr_accessor :user_id, :password, :student_id
     def initialize()
 
     end
 
     def self.new_session()
+        login
+    end
 
+    def login
+
+    end
+    def self.display_and_get_input
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
     has_many :chatrooms, through: :userchatrooms
     has_many :messages
     
+    #checks if user with given username exists
+    def self.username_taken(username)
+        return !!(User.find_by(username: username))
+    end
+
 end
